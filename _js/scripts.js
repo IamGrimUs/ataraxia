9b553e7924f67c1bed54696ba8d63e43b619ace1
Russell Thomas <iamgrimus@gmail.com>
2017-06-24 21:46:08 -0700

    image loading without loop

--- _js/scripts.js
+++ _js/scripts.js
@@ -17,21 +17,18 @@ var placeHolder = 0;
 //on focus of menu button show the photographer name and photo location
 $(MENU_BUTTON).focus(function() {
   PHOTO_CREDITS.classList.toggle('hidden')
-});
+  });
 
 //on hover of menu button show the photographer name and photo location
 $(MENU_BUTTON).hover(function() {
   PHOTO_CREDITS.classList.toggle('hidden')
-});
+  });
 
 //display the photo & details on the screen
 function displayContent () {
-  $('.js-image-name').prepend(imageName[placeHolder]);
-  $('.js-location-city').prepend(locationCity[placeHolder] + ',');
-  $('.js-location-city').append(locationCountry[placeHolder]);
-  $('.js-photographer-name').append(firstNameList[placeHolder] + ' ');
-  $('.js-photographer-name').append(lastNameList[placeHolder]);
-  $('body').css('background-image', 'url("' + imageList[placeHolder] + '")');
+  $('.js-image-name').html(imageName[placeHolder]);
+  $('.js-location-city').html(locationCity[placeHolder] + ', ' + locationCountry[placeHolder]);
+  $('.js-photographer-name').html(firstNameList[placeHolder] + ' ' + lastNameList[placeHolder]);
   }
 
 //push the unique photo information into the variable arrays
@@ -39,9 +36,8 @@ function displayIds(data) {
   imageName.push(data.location.name);
   locationCity.push(data.location.city);
   locationCountry.push(data.location.country);
-  console.log('location city is: ' + locationCity);
-  console.log('Image name is: ' + imageName);
-  displayContent();
+  // console.log('location city is: ' + locationCity);
+  // console.log('Image name is: ' + imageName);
   };
 
 //pass the unique photo id tags back to the api and request more detailed information
@@ -49,21 +45,21 @@ function processLocation(data) {
   // console.log('This is processLocation data: ' + data);
   data.forEach(function(i){
     var UNSPLASH_ID = 'https://api.unsplash.com/photos/'+ i +'/?client_id=cc682cb24c6d39c5f80d0e5b0703f516c2ce7e7a4c5028787549cc061e8b8880';
-    $.getJSON(UNSPLASH_ID).then(displayIds);
+    $.getJSON(UNSPLASH_ID).then(displayIds).then(displayContent);
     });
   };
 
 //use getJSON call to the unsplash API requesting a collection of images
 function getImage() {
   var query = {
-    per_page: 3,
+    per_page: 2,
     }
-  $.getJSON(UNSPLASH_URL, query).then(showMeData).then(processLocation);
+  $.getJSON(UNSPLASH_URL, query).then(showMeData).then(processLocation).then(imageBackgroundSlideShow);
   };
 
 //initial call back function populates the array of returned images, the photographers name & the photo IDs
 function showMeData(data) {
-  console.log('these are the photos return from unsplash:');
+  console.log('these are the photos returned from unsplash:');
   console.log(data);
 
   //use for each function to pull desired objects from unsplash and push them to the variable arrays
@@ -73,8 +69,25 @@ function showMeData(data) {
     lastNameList.push(i.user.last_name);
     photoId.push(i.id);
     });
-
   return photoId;
   };
 
+//add the full url from unsplash to the background of the body tag
+function imageShow() {
+  console.log(imageList);
+  $('body').css('background-image', `url(${imageList[placeHolder]})`);
+  }
+
+//attempt to loop through the imageList array and display each image for 15 seconds
+function imageBackgroundSlideShow() {
+  // console.log(imageList[0]);
+  // console.log(imageList[1]);
+  imageShow()
+  }
+
 $(getImage);
+
+
+
+
+
