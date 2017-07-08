28a99eece0eadbb2b478694f6e29f0eca1e165a6
Russell Thomas <iamgrimus@gmail.com>
2017-07-07 17:53:04 -0700

    preloader and slide show complete builds

--- _js/app.js
+++ _js/app.js
@@ -6,74 +6,34 @@ const STATE = {
   images: []
 };
 
-function displaySlideShow(backgroundImage, portfolioUrl, firstName, lastName) {
-  console.log("displaySlideShow fire...");
-  // console.log(backgroundImage);
-  let imageBlock = `<li>
-            <span style="background-image: url(${backgroundImage})"></span>
-            <div>
-            <p><a href="${portfolioUrl}">${firstName} ${lastName}</a></p>
-            </div>
-          </li>`;
-  $(".slideshow").append(imageBlock);
-}
-
-//capture the image object properties and display them in the html
-function showContent() {
-  console.log("showing content...");
-  const state = STATE;
-  for (var image of state.images) {
-    let backgroundImage = image.urls.full;
-    let firstName = image.user.first_name;
-    let lastName = image.user.last_name;
-    let portfolioUrl = image.user.portfolio_url;
-    // let locationName = _.get(image, "details.location.name", "");
-    // let locationCity = _.get(image, "details.location.city", "");
-    // let locationCountry = _.get(image, "details.location.country", "");
-    displaySlideShow(backgroundImage, portfolioUrl, firstName, lastName);
-  }
-}
-
 //use getJSON call to the unsplash API requesting a collection of images
 function callUnsplashAPI() {
   let query = {
-    per_page: 3
+    per_page: 2
   };
   $.getJSON(COLLECTIONS_URL, query)
-    .then(saveData)
-    .then(shuffle)
-    .then(showContent)
-    .then(hidePreloader);
+    .then(saveApiData)
+    .then(shuffleImageArray)
+    .then(saveApiObjectProperties)
+    .then(render);
 }
 
 //initial callback function populates the array of returned images, the photographers name & the photo IDs
-function saveData(data) {
+function saveApiData(data) {
   STATE.images.push(...data);
   console.log("STATE.images:");
   console.log(STATE.images);
   return STATE.images;
-  // showContent();
-  // getDetails(STATE);
-  // saveDetails(STATE, MORE_INFO);
 }
 
-function hidePreloader() {
-  console.log("hidding...");
-  $(".spinner-wrapper").addClass("hide-preloader");
-}
-
-function shuffle(array) {
-  // console.log("array:");
-  // console.log(array);
+function shuffleImageArray(array) {
   let currentIndex = array.length,
     temporaryValue,
     randomIndex;
 
   // While there remain elements to shuffle...
   while (0 !== currentIndex) {
-    // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
-    console.log(randomIndex);
     currentIndex -= 1;
 
     // And swap it with the current element.
@@ -81,35 +41,76 @@ function shuffle(array) {
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
   }
-  console.log("shuffled array:");
-  console.log(array);
   return array;
 }
 
-//pass the unique photo id tags back to the api and request more detailed inform ation
-// function getDetails(state) {
-//   const promises = [];
-//   for (let i = 0; i < state.images.length; i++) {
-//     let PHOTO_URL = BASE_URL + "photos/" + state.images[i].id + "/?" + ID_STR;
-//     const saveDetailsPromise = $.getJSON(PHOTO_URL).then(saveDetails);
-//     promises.push(saveDetailsPromise);
-//   }
-//   Promise.all(promises).then(showContent);
-// }
+//capture the image object properties and display them in the html
+function saveApiObjectProperties() {
+  console.log("showing content...");
+  const state = STATE;
+  let currentIndex = 0;
+  for (var image of state.images) {
+    let backgroundImage = image.urls.full;
+    let firstName = image.user.first_name;
+    let lastName = image.user.last_name;
+    let portfolioUrl = image.user.portfolio_url;
+    currentIndex = currentIndex + 1;
+    appendHTMLElement(backgroundImage, portfolioUrl, firstName, lastName);
+  }
+}
 
-//match the id property in STATE.images to id property contained in the data array
-// function saveDetails(data) {
-//   const state = STATE;
-//   for (let i = 0; i < data.length; i++) {
-//     let imgMatch = state.images.find(function(image, index) {
-//       return image.id === data[i].id;
-//     });
-//     imgMatch.details = data[i];
-//   }
-//showContent(state);
-// }
+function render() {
+  cacheFirstImage(displayImageSlideShow);
+}
+
+function cacheFirstImage(callback) {
+  let image = new Image();
+  image.onload = function() {
+    callback();
+  };
+  image.src = STATE.images[0].urls.full;
+}
+
+function displayImageSlideShow(uniqueImageId) {
+  console.log("slideshow time...");
+  let slideIndex = 0;
+  showSlides();
+  function showSlides() {
+    let slides = document.getElementsByClassName("js-slideshowFrame");
+    console.log(slides[0]);
+    for (let i = 0; i < slides.length; i++) {
+      $(slides[i]).addClass("slideshow-image--hide");
+      $(slides[i]).removeClass("slideshow-image--reveal");
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
+    $(slides[slideIndex - 1]).addClass("slideshow-image--reveal");
+    $(slides[slideIndex - 1]).removeClass("slideshow-image--hide");
+    setTimeout(showSlides, 90000); // Change image every 90 seconds
+  }
+  hidePreloader();
+}
+
+function hidePreloader() {
+  console.log("hidding...");
+  $(".spinner-wrapper").addClass("hide-preloader");
+}
+
+//add the images into the html for slideshow display
+function appendHTMLElement(backgroundImage, portfolioUrl, firstName, lastName) {
+  console.log("appending li...");
+  let imageBlock = `<li class="slideshow-image--hide js-slideshowFrame">
+            <span style="background-image: url(${backgroundImage})"></span>
+            <div>
+            <p class="photographer-info"><a href="${portfolioUrl}"><i class="fa fa-camera-retro" id="camera-icon" aria-hidden="true"></i> by ${firstName} ${lastName}</a></p>
+            <p class="disclaimer">provided by unsplash</p>
+            </div>
+          </li>`;
+  $(".slideshow").append(imageBlock);
+}
 
 $(function() {
   callUnsplashAPI();
-  // saveData(PHOTO_DATA);
 });
